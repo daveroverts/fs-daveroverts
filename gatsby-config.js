0b8bd4b26b9ba9672c1cc268af2ef2ecbb629cd9
Dave Roverts <me@daveroverts.nl>
2021-03-31 12:22:26 +0200

    Removed service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     `gatsby-plugin-gatsby-cloud`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,

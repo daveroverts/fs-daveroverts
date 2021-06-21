58eeb1a13d9ddd582b3e06b0a65108d0959f3082
Dave Roverts <me@daveroverts.nl>
2021-06-21 20:01:12 +0200

    Use preact for production. Package updates

--- next.config.js
+++ next.config.js
@@ -1,17 +1,29 @@
 module.exports = {
-    reactStrictMode: true,
-    async redirects() {
-      return [
-        {
-          source: '/twitter',
-          destination: 'https://twitter.com/daveroverts',
-          permanent: true,
-        },
-        {
-          source: '/youtube',
-          destination: 'https://youtube.com/daveroverts',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/daveroverts',
+        permanent: true,
+      },
+      {
+        source: '/youtube',
+        destination: 'https://youtube.com/daveroverts',
+        permanent: true,
+      },
+    ]
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
+}

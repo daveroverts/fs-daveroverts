208815f451ecd506ace35653d138ef059a8b3357
Dave Roverts <me@daveroverts.nl>
2021-07-03 22:33:38 +0200

    Add green/red circle before callsign. Now it doesn't allign with rest...

--- components/VatsimStatusIndicator.js
+++ components/VatsimStatusIndicator.js
@@ -6,12 +6,13 @@ export const VatsimStatusIndicator = () => {
     const { data } = useSWR(`/api/vatsim/online/${cid}`, fetcher);
 
     if (!data) {
-      return null
+        return null
     }
 
     return (
-        <a className="px-3 py-2 bg-gray-100 rounded-md cursor-default">
-            {data.callsign ? data.callsign : 'Offline'} {data.depArr ? `| ${data.depArr}` : null}
-        </a>
+        <div className="flex justify-around px-3 py-2 space-x-1 bg-gray-100 rounded-md cursor-default">
+            <div className={`self-center w-3 h-3 rounded-full ${data.callsign ? 'bg-green-500' : 'bg-red-500'}`}></div>
+            <div className="">{data.callsign ? data.callsign : 'Offline'} {data.depArr ? `| ${data.depArr}` : null}</div>
+        </div>
     )
 }

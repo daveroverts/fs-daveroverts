8bff591c6e9fa3913c27f244d4916ead7e5a4fc1
Dave Roverts <me@daveroverts.nl>
2021-07-04 18:23:03 +0200

    WIP Show image if it exists

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,17 +1,34 @@
 import { format, parseISO } from "date-fns";
 import { enGB } from "date-fns/locale";
 import Link from 'next/link';
+import Image from 'next/image'
 
 const BlogPost = ({ post }) => {
 
+    console.log(post)
+
     return (
-        <article className="flex flex-col justify-center min-h-full px-5 py-2 text-center transition border-2 rounded-full hover:bg-gray-50 dark:hover:bg-gray-800">
+        <article className="flex flex-col justify-center min-h-full text-center transition border-2 rounded-full hover:bg-gray-50 dark:hover:bg-gray-800">
             <Link href={`/posts/${post.slug}`}>
                 <a className="hover:underline">
-                    <h4 className="text-xl">{post.title}</h4>
-                    <h5 className="text-lg">
-                        {format(parseISO(post.date), 'P', { locale: enGB })}
-                    </h5>
+                    {post.banner ? (
+                        <div className="relative w-full h-80">
+                            <Image
+                                src={post.banner}
+                                alt={post.title}
+                                layout="fill"
+                                objectFit="cover"
+                                className="rounded-full"
+                            />
+                        </div>
+                    ) : (
+                        <>
+                            <h4 className="text-xl">{post.title}</h4>
+                            <h5 className="text-lg">
+                                {format(parseISO(post.date), 'P', { locale: enGB })}
+                            </h5>
+                        </>
+                    )}
                 </a>
             </Link>
         </article>

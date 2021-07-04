288a12d88274b18abfa0b2054d159d7113ff8d17
Dave Roverts <me@daveroverts.nl>
2021-07-04 11:01:06 +0200

    Always render VatsimStatusIndicator, even if there's no data yet

--- components/VatsimStatusIndicator.js
+++ components/VatsimStatusIndicator.js
@@ -5,14 +5,10 @@ export const VatsimStatusIndicator = () => {
     const cid = 1186831;
     const { data } = useSWR(`/api/vatsim/online/${cid}`, fetcher);
 
-    if (!data) {
-        return null
-    }
-
     return (
         <div className="flex justify-around px-3 py-2 space-x-1 bg-gray-100 rounded-md cursor-default">
-            <div className={`self-center w-3 h-3 rounded-full ${data.callsign ? 'bg-green-500' : 'bg-red-500'}`}></div>
-            <div className="">{data.callsign ? data.callsign : 'Offline'} {data.depArr ? `| ${data.depArr}` : null}</div>
+            <div className={`self-center w-3 h-3 rounded-full ${data?.callsign ? 'bg-green-500' : 'bg-red-500'}`}></div>
+            <div className="">{data?.callsign ? data.callsign : 'Offline'} {data?.depArr ? `| ${data?.depArr}` : null}</div>
         </div>
     )
 }

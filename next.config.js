c98cadee3c4fd9568990d422f3a5877a8f52ab60
Dave Roverts <me@daveroverts.nl>
2021-07-04 20:19:17 +0200

    Add most security headers from Next.js documentation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,45 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value: ''// Your CSP Policy
+  // }
+]
+
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
   async redirects() {
     return [
       {

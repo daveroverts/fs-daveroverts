cf7f745976c7349e3a098de4ec1899faa7d534fd
Dave Roverts <me@daveroverts.nl>
2021-08-01 14:55:58 +0200

    For now only show border if there is no banner

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -7,7 +7,7 @@ const BlogPost = ({ post, index }) => {
     return (
         <Link href={`/posts/${post.slug}`}>
             <a className="hover:underline">
-                <article className={`transform hover:scale-105 flex flex-col justify-center min-h-full text-center transition rounded-3xl hover:bg-gray-50 dark:hover:bg-gray-800 ${!post.banner ? 'border-2' : ''} ${index % 2 ? 'hover:rotate-1' : 'hover:-rotate-1'}`}>
+                <article className={`transform hover:scale-105 flex flex-col justify-center min-h-full text-center transition rounded-3xl ${!post.banner ? 'border-2 hover:bg-gray-50 dark:hover:bg-gray-800' : ''} ${index % 2 ? 'hover:rotate-1' : 'hover:-rotate-1'}`}>
                     {post.banner ? (
                         <div className="relative h-64 w-96 xl:w-full xl:h-96">
                             <Image

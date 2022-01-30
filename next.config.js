18b3ef3a2bd4173d55cbbafb4b4f2f7377333f1c
Dave Roverts <me@daveroverts.nl>
2022-01-30 23:14:57 +0100

    Use env value within CSP

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const { withPlaiceholder } = require('@plaiceholder/next')
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com fifteen-twentytwo.daveroverts.nl;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com ${process.env.NEXT_PUBLIC_FATHOM_URL};
   child-src 'self';
   style-src 'self' 'unsafe-inline';
-  img-src * blob: data: i.ytimg.com fifteen-twentytwo.daveroverts.nl;
+  img-src * blob: data: i.ytimg.com ${process.env.NEXT_PUBLIC_FATHOM_URL};
   object-src 'none';
   media-src 'none';
   frame-src www.youtube-nocookie.com;

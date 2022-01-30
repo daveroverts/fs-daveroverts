40bf6ef22793b6d902839a212b1d61f9d9b61c42
Dave Roverts <me@daveroverts.nl>
2022-01-30 23:01:08 +0100

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,17 @@ import MDXComponents from "../components/MDXComponents";
 import { DefaultSeo } from "next-seo";
 import SEO from '../next-seo.config'
 import 'tailwindcss/tailwind.css'
+import { useAnalytics } from 'lib/analytics';
 
 function MyApp({ Component, pageProps }) {
+  useAnalytics();
+
   return (
     <>
       <ThemeProvider
-      attribute="class"
-      storageKey="nightwind-mode"
-      defaultTheme="system"
+        attribute="class"
+        storageKey="nightwind-mode"
+        defaultTheme="system"
       >
         <DefaultSeo {...SEO} />
         <MDXProvider components={MDXComponents}>

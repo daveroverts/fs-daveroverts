b13887457009ad2f67c3089a4c0e0dadbee112d2
Dave Roverts <me@daveroverts.nl>
2022-06-06 14:28:57 +0200

    Fix copyright date

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,7 @@
-import Emoji from "a11y-react-emoji";
-
 export default function Footer() {
     return (
         <footer className="flex items-center justify-center w-full pt-5 text-sm text-center">
-            © 2021 {new Date().getFullYear() > 2021 ? "- 2021" : ""} by FS Dave
-            Roverts <Emoji className="font-medium" symbol="✈️" />. All rights reserved.
+            © 2021 - {new Date().getFullYear()} by Dave Roverts. All rights reserved.
         </footer>
     )
 }

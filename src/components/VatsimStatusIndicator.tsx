0f023b8d2ea40a14c20ac8a7399b89f59fad4a6b
Dave Roverts <me@daveroverts.nl>
2025-04-21 19:36:37 +0200

    Refactor to use src directory and absolute imports

--- src/components/VatsimStatusIndicator.tsx
+++ src/components/VatsimStatusIndicator.tsx
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import fetcher from "lib/fetcher";
+import fetcher from "@/lib/fetcher";
 import Emoji from "a11y-react-emoji";
 
 export const VatsimStatusIndicator = () => {

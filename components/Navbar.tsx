5214f2573923d2d5229f6b363d126455639ad5e0
Dave Roverts <me@daveroverts.nl>
2025-04-21 15:28:26 +0200

    Add new logo / favicon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import { VatsimStatusIndicator } from "./VatsimStatusIndicator";
 import { useRouter } from "next/router";
+import logo from "../public/logo.png";
+import Image from "next/image";
 
 export const Navbar = () => {
   const router = useRouter();
@@ -22,20 +24,32 @@ export const Navbar = () => {
     },
   ];
 
-
   const NAV_ITEMS_RIGHT = [
     {
       label: "Youtube",
       href: "https://youtube.com/daveroverts",
-    }
+    },
   ];
 
   return (
     <div className="px-4 py-8 border-b-2">
       <div className="flex justify-between">
         <div>
-          <h1 className="pb-4 text-4xl font-bold dark:text-white">
-            FS Dave Roverts <Emoji className="font-medium" symbol="✈️" />
+          <h1 className="flex flex-row items-center justify-center pb-4 space-x-2 text-4xl font-bold dark:text-white">
+            <div>
+              <Image
+                src={logo}
+                alt="FS Dave Roverts Logo"
+                width={30}
+                height={30}
+                className="w-auto h-10 transition-all duration-300 dark:invert"
+                priority
+              />
+            </div>
+            <div>FS Dave Roverts</div>
+            <div>
+              <Emoji className="font-medium" symbol="✈️" />
+            </div>
           </h1>
         </div>
         <div>
@@ -48,7 +62,12 @@ export const Navbar = () => {
             <Link
               key={item.label}
               href={item.href}
-              className={`px-3 py-2 rounded-md ${router.pathname === item.href ? 'bg-gray-200 dark:bg-gray-700 dark:text-white' : 'hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white'}`}>
+              className={`px-3 py-2 rounded-md ${
+                router.pathname === item.href
+                  ? "bg-gray-200 dark:bg-gray-700 dark:text-white"
+                  : "hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
+              }`}
+            >
               {item.label}
             </Link>
           ))}
@@ -59,7 +78,12 @@ export const Navbar = () => {
             <Link
               key={item.label}
               href={item.href}
-              className={`px-3 py-2 rounded-md ${router.pathname === item.href ? 'bg-gray-200 dark:bg-gray-700 dark:text-white' : 'hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white'}`}>
+              className={`px-3 py-2 rounded-md ${
+                router.pathname === item.href
+                  ? "bg-gray-200 dark:bg-gray-700 dark:text-white"
+                  : "hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
+              }`}
+            >
               {item.label}
             </Link>
           ))}
@@ -67,4 +91,4 @@ export const Navbar = () => {
       </div>
     </div>
   );
-}
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,23 +7,20 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png?v=2"
+            sizes="96x96"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+          <link rel="shortcut icon" href="/favicon.ico?v=2" />
+          <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/apple-touch-icon.png?v=2"
           />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="apple-mobile-web-app-title" content="FS DR" />
+          <link rel="manifest" href="/site.webmanifest?v=2" />
         </Head>
         <body className="dark:bg-gray-900">
           <Main />

4ece84204ab5098bda4e0092f9e333ceca2f6855
Dave Roverts <me@daveroverts.nl>
2025-04-22 19:58:19 +0200

    Upgrade to next 15 and react 19

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import { ReactElement } from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render(): ReactElement {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicon-96x96.png?v=2"
-            sizes="96x96"
-          />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
-          <link rel="shortcut icon" href="/favicon.ico?v=2" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png?v=2"
-          />
-          <meta name="apple-mobile-web-app-title" content="FS DR" />
-          <link rel="manifest" href="/site.webmanifest?v=2" />
-        </Head>
-        <body className="dark:bg-gray-900">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png?v=2"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=2"
+        />
+        <meta name="apple-mobile-web-app-title" content="FS DR" />
+        <link rel="manifest" href="/site.webmanifest?v=2" />
+      </Head>
+      <body className="dark:bg-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
